df4eacd6a788574252c4770ef211fbfe825ec0e3
Carolina Tacconis <ctacconi@cbr4s3.42barcelona.com>
2024-02-09 20:37:54 +0100

    Ft_printf

---
+++ ft_printf.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctacconi <ctacconi@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/02 19:56:00 by ctacconi          #+#    #+#             */
+/*   Updated: 2024/02/09 17:29:06 by ctacconi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft/libft.h"
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+
+int		ft_printf(char const *formato, ...);
+void	ft_put_unbr(unsigned int n, int *len);
+void	ft_putchar(char c, int *len);
+void	ft_putstr(char *s, int *len);
+void	ft_putnbr(int number, int *len);
+void	ft_puthex(unsigned int num, char c, int *len);
+void	ft_putptr(unsigned long ptr, int *len);
+
+#endif
---
+++ ft_putnbr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctacconi <ctacconi@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/06 22:28:46 by ctacconi          #+#    #+#             */
+/*   Updated: 2024/02/09 17:34:55 by ctacconi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putnbr(int number, int *len)
+{
+	char	*num;
+
+	num = ft_itoa(number);
+	if (!num)
+	{
+		*len = -1;
+		return ;
+	}
+	ft_putstr(num, len);
+	free(num);
+}
+
+/*int	main(void)
+{
+	//printf("%d", printf("%d", -1));	
+	//print('\n');
+
+	printf("%d\n", printf("%d", -10));
+	print('\n');
+
+	ft_printf("%d\n", printf("%d", -10));
+	//print('\n');
+
+	//printf("%d", printf("%d", -11));
+	//print('\n');
+
+	//printf("%d", printf("%d", -14));
+	//print('\n');
+}*/
